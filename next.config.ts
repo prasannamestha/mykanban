99168456b3d1947fa647d2809d5ab02e808bc61c
Prasanna Mestha <prasannamesta@gmail.com>
2024-12-13 17:14:50 +0200

    add asset prefix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  assetPrefix: 'https://www.cutekanban.com/',
 };
 
 export default nextConfig;
