790c01081aafef00f35c80396ad8003e32f4084f
Prasanna Mestha <prasannamesta@gmail.com>
2024-12-13 18:07:04 +0200

    add posthog

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
+import { Suspense } from 'react';
+import { PHProvider, PostHogPageview } from './providers';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -25,11 +27,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gradient-to-b from-zinc-900 to-neutral-950 text-white min-h-screen`}
-      >
-        {children}
-      </body>
+      <Suspense>
+        <PostHogPageview />
+      </Suspense>
+      <PHProvider>
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gradient-to-b from-zinc-900 to-neutral-950 text-white min-h-screen`}
+        >
+          {children}
+        </body>
+      </PHProvider>
     </html>
   );
 }
---
+++ src/app/providers.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import { ReactNode, useEffect } from 'react';
+import { usePathname, useSearchParams } from 'next/navigation';
+import posthog from 'posthog-js';
+import { PostHogProvider } from 'posthog-js/react';
+
+if (typeof window !== 'undefined') {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    persistence: 'localStorage',
+  });
+}
+
+export function PostHogPageview() {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  // Track pageviews
+  useEffect(() => {
+    if (pathname) {
+      let url = window.origin + pathname;
+      if (searchParams.toString()) {
+        url = url + `?${searchParams.toString()}`;
+      }
+      posthog.capture('$pageview', {
+        $current_url: url,
+      });
+    }
+  }, [pathname, searchParams]);
+  return null;
+}
+
+export function PHProvider({ children }: { children: ReactNode }) {
+  return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
+}
