9985905e5e512461eea1182cd4fb58dbde75c8ca
Prasanna Mestha <prasannamesta@gmail.com>
2024-12-17 16:31:57 +0200

    revert asset

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // assetPrefix: 'https://www.cutekanban.com/',
+  assetPrefix: 'https://www.cutekanban.com/',
 };
 
 export default nextConfig;
